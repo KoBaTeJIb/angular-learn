038101de22bd956e6c6b2ff89e4e981223550407
KoBaTeJIb <sazonov.yurij@gmail.com>
2018-02-01 10:13:00 +0300

    to be continued1

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  searchStr: string = '';
+  minLength: number = 3;
+
+  constructor() { }
+
+  handleChange() {
+    console.log(this.searchStr);
+  }
+
+  ngOnInit() {
+  }
+
+}
