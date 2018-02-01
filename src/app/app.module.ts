0f53316703246e406d83fe2a168889ebaa780021
KoBaTeJIb <sazonov.yurij@gmail.com>
2018-02-01 09:47:57 +0300

    to be continued

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-
+import {MatToolbarModule, MatInputModule, MatCardModule, MatButtonModule} from '@angular/material';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import {FormsModule} from '@angular/forms';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ToolbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
