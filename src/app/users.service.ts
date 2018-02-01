0560e3c644b7ca4c6fa0da2ad60be826ed159915
KoBaTeJIb <sazonov.yurij@gmail.com>
2018-02-01 15:30:50 +0300

    final

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -7,8 +7,8 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class UsersService {
   constructor(private http: HttpClient) {}
-    getUser(username: string): Observable <any> {
-    return this.http.get('https://api.github.com/users/$(username)')
+    getUser(username: string): Observable<any> {
+    return this.http.get(`https://api.github.com/users/${username}`)
       //.map((response: Response) => response.json())
     }
 
